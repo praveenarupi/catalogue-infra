aaae8c9503130a7b31efdbf8441048f30cf67f11
praveenarupi <praveenarupi@gmail.com>
2022-11-23 13:11:21 +0530

    updating ./infra/catalogue-infra

--- immutable/main.tf
+++ immutable/main.tf
@@ -3,7 +3,7 @@ module "catalogue" {
   COMPONENT        = var.COMPONENT
   ENV              = var.ENV
   APP_PORT = 8080
-  LB_RULE_PRIORITY = 1
+  LB_RULE_PRIORITY = 2
   APP_VERSION = var.APP_VERSION
   desired_capacity = var.desired_capacity
   max_size         = var.max_size
